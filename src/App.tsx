6156c60bfac0d8da959ae601c420c88d477c0586
minheewon <lkmj12qw@gmail.com>
2025-12-27 21:04:03 +0900

    [🎨 style] Footer UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./components/about/About";
 import Education from "./components/education/Education";
 import Hero from "./components/hero/Hero";
+import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/Navbar";
 import Projects from "./components/projects/Projects";
 import Skills from "./components/skills/Skills";
@@ -16,6 +17,7 @@ function App() {
       <Work />
       <Education />
       <Projects />
+      <Footer />
     </>
   );
 }
